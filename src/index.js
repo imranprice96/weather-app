efaad4f6271425d02ce5f69bf5fadbba7a412873
Imran Price <imranprice96@gmail.com>
2022-09-17 23:07:15 +1200

    Fix weather API handler

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import weatherApihandler from "./modules/weatherApihandler";
+
+async function main() {
+  const a = await weatherApihandler.getWeatherData("Auckland", "Metric");
+  console.log(a);
+}
+
+main();
